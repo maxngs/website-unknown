e4c82779f4acf4f3dc2de3bc46e2a542d121f02d
maxime nogues <maxime.nogues1408@gmail.com>
2026-03-03 23:34:47 +0100

    Correcting the path mistake for company page

--- app/entreprises/page.tsx
+++ app/entreprises/page.tsx
@@ -1,33 +1,32 @@
+// app/ecoles/page.tsx
 "use client";
 
 import { Navbar } from "../components/layout/Navbar";
 import { Footer } from "../components/layout/Footer";
-import {
-  HeroBackground,
-  HeroSection,
-  TrustBand,
-  PainPointSection,
-  SolutionSection,
-  HowItWorks,
-  PricingSection,
-  FAQSection,
-  FinalCTA,
-  globalKeyframes,
-} from "../components/entreprises";
-import EventBanner from "../components/shared/EventBanner";
+import HeroBackground from "../components/entreprises/HeroBackground";
+import HeroSection from "../components/entreprises/HeroSection";
+import TrustBand from "../components/entreprises/TrustBand";
+import PainPointSection from "../components/entreprises/PainPointSection";
+import SolutionSection from "../components/entreprises/SolutionSection";
+import HowItWorks from "../components/entreprises/HowItWorks";
+// import PricingSection from "../components/entreprises/PricingSection";
+import FAQSection from "../components/entreprises/FAQSection";
+import FinalCTA from "../components/entreprises/FinalCTA";
+import { globalKeyframes } from "../components/entreprises/styles";
+import BeforeAfterSection from "../components/entreprises/BeforeAfterSection";
 
 export default function EntreprisesPage() {
   return (
     <div className="relative min-h-screen bg-[#fafafa]" style={{ overflowX: "clip" }}>
-      <Navbar ctaLabel="Devenir partenaire" />
-      <EventBanner />
+      <Navbar ctaLabel="Publier une offre" />
       <HeroBackground />
       <HeroSection />
       <TrustBand />
       <PainPointSection />
       <SolutionSection />
       <HowItWorks />
-      <PricingSection />
+      {/* <PricingSection /> */}
+      <BeforeAfterSection />
       <FAQSection />
       <FinalCTA />
       <Footer />
